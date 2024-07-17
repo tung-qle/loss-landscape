a0408c1f788b96580554696ac2392e8123e05d66
tung-qle <quoctung.nus@gmail.com>
2024-07-17 15:51:31 +0200

    Add requirements file

---
+++ script/1d_rd_linear_inter_mlp.sh
@@ -0,0 +1,2 @@
+python plot_1D.py --surf_file /home/tung/Postdoc1/loss-landscape/cifar10/trained_nets/mlp/random_mlp_random_direction.h5_[-3.0,3.0,2000].h5 \
+    --log --xmin -3 --xmax 3 --loss_max 10
